f2fbf8d9463509215552358f34083d7c59a1cd6e
ggod <46885632+GGodPL@users.noreply.github.com>
2025-04-18 13:00:23 +0000

    config changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
     base: '/discord/',
     envDir: '../',
     server: {
@@ -12,10 +13,27 @@ export default defineConfig({
                 secure: false,
                 ws: true,
             },
+=======
+    base: "/discord/",
+    envDir: './',
+    server: {
+        proxy: {
+        '/api': {
+            target: "http://localhost:3010",
+            changeOrigin: true,
+            secure: false,
+            ws: true,
+        }
+>>>>>>> Stashed changes
         },
         hmr: {
+	    protocol: "wss",
+            host: "sandboxels.live",
             clientPort: 443,
         },
         allowedHosts: true,
+        port: 5173,
+        https: false,
+        origin: "https://sandboxels.live"
     },
 });
